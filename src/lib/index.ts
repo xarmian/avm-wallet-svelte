190fd92902e6907a0efcf2fad47d00ae6818d4c0
xarmian <xarmian@gmail.com>
2024-05-08 19:36:50 -0400

    fix export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 // Reexport your entry components here
 export { default as Web3Wallet } from './Web3Wallet.svelte';
-export { selectedWallet, connectedWallets } from './store.js';
+export { selectedWallet, connectedWallets, setOnAddHandler, setOnAuthHandler } from './store.js';
 export { getSelectedWalletToken, signAndSendTransactions, signTransactions, Wallets as WalletOptions, verifyToken } from './wallets.js';
 export type { WalletConnectionResult } from './wallets.js';
-export type { AVMWalletStore, SelectedWalletStore, setOnAddHandler, setOnAuthHandler } from './store.js';
+export type { AVMWalletStore, SelectedWalletStore } from './store.js';
